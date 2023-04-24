ebc24aeceed6d8a81ad3be25fb39d37f00746b46
KorbiS96 <126156705+KorbiS96@users.noreply.github.com>
2023-04-24 16:11:45 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#py -m pip install scikit-image
+from dash import Dash, html
+from source.components.layout import create_layout
+import dash_bootstrap_components as dbc
+
+def main () -> None:
+    app = Dash(__name__)
+    server = app.server
+    app.title = "GFCC Database"
+    app.layout = create_layout(app)
+    app.run()
+
+if __name__ == "__main__":
+    main() 
